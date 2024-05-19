469650695ec0da0c1d833a56fd76ef4142e00598
carina <ecarina.gonzalez@gmail.com>
2024-05-18 20:00:44 -0600

    [feature-001] structure project and routes :sparkles:

---
+++ project_mac_orders/src/middleware/fetchData.tsx
@@ -0,0 +1,42 @@
+import { FetchData } from "../utils/interfaces";
+
+export const getData = async () => {
+  try {
+  const res = await fetch("http://localhost:3000/api/products");
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+} catch (error) {
+  console.error(`Error fetching get data: ${error}`);
+  return null;
+}
+};
+
+// Mock function to simulate fetching post data
+export const fetchPost = async (slug: string) => {
+  try {
+    const res = await getData();
+    if (!res) {
+      console.error(`Failed to fetch post data for slug: ${slug}`);
+      return null;
+    }
+
+    const products = res?.data?.products;
+    if (!products || !Array.isArray(products)) {
+      console.error(`Invalid response data format for slug: ${slug}`);
+      return null;
+    }
+
+    const product = products.find(
+      (prod: { slug: string }) => prod.slug === slug
+    );
+
+    return product;
+  } catch (error) {
+    console.error(`Error fetching post data for slug: ${slug}`, error);
+    return null;
+  }
+};
