6fa76290b72c59176ebb6792efc0b0a531a602c6
carina <ecarina.gonzalez@gmail.com>
2024-05-19 21:57:17 -0600

    [feature-009] trying to fix deploy Vercel

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,12 +1,12 @@
-export async function GET() {
+export async function GET(req, res) {
   try {
     const response = await fetch(
       "https://burgerhub00.github.io/data/products.json"
     );
     const responsFull = await response.json();
-    return { statusCode: 200, body: JSON.stringify({ data: responsFull }) };
+    res.status(200).json({ data: responsFull });
   } catch (error) {
-    console.log("Error fetching post data");
-    return { statusCode: 400, error: error };
+    console.error("Error fetching product data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 }
