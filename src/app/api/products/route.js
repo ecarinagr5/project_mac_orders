0a48890feeb60ee709986e209cc468380304113b
carina <ecarina.gonzalez@gmail.com>
2024-05-19 21:53:21 -0600

    [feature-009] trying to fix deploy Vercel

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,16 +1,12 @@
-import { NextResponse } from "next/server";
-
 export async function GET() {
   try {
     const response = await fetch(
       "https://burgerhub00.github.io/data/products.json"
     );
     const responsFull = await response.json();
-    return NextResponse.json({
-      data: responsFull,
-    });
+    return { statusCode: 200, body: JSON.stringify({ data: responsFull }) };
   } catch (error) {
     console.log("Error fetching post data");
-    return error;
+    return { statusCode: 400, error: error };
   }
 }
