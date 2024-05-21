8d3cdd323393e916d0b4730ae0eb742254f509b9
carina <ecarina.gonzalez@gmail.com>
2024-05-21 11:26:07 -0600

    [f-0012] aunthentication and README update

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,3 @@
-
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
@@ -40,4 +39,4 @@ const authOptions = {
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST, authOptions };
+export { handler as GET, handler as POST };
